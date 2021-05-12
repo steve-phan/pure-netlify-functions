d331042289b5e584e990dfaf46cc3dd3585661b5
vUnAmp <phanhaingoc@gmail.com>
2021-05-12 20:25:53 +0200

    okey babe

--- functions/hello-world/hello-world.js
+++ functions/hello-world/hello-world.js
@@ -1,6 +1,5 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event, context) => {
-  console.log(event);
   try {
     const subject = event.queryStringParameters.name || 'World';
     let mode = event.queryStringParameters['hub.mode'];
@@ -26,25 +25,25 @@ const handler = async (event, context) => {
 
 module.exports = { handler };
 
-app.get('/webhook', (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = '<YOUR_VERIFY_TOKEN>';
+// app.get('/webhook', (req, res) => {
+//   // Your verify token. Should be a random string.
+//   let VERIFY_TOKEN = '<YOUR_VERIFY_TOKEN>';
 
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+//   // Parse the query params
+//   let mode = req.query['hub.mode'];
+//   let token = req.query['hub.verify_token'];
+//   let challenge = req.query['hub.challenge'];
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
-});
+//   // Checks if a token and mode is in the query string of the request
+//   if (mode && token) {
+//     // Checks the mode and token sent is correct
+//     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+//       // Responds with the challenge token from the request
+//       console.log('WEBHOOK_VERIFIED');
+//       res.status(200).send(challenge);
+//     } else {
+//       // Responds with '403 Forbidden' if verify tokens do not match
+//       res.sendStatus(403);
+//     }
+//   }
+// });
